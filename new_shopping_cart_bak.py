be27b4de0ca2ce320e16361725d0878b8b273a32
Summer <xiaoqisl@iCloud.com>
2017-10-13 17:47:47 +0800

    Add files via upload

---
+++ new_shopping_cart_bak.py
@@ -0,0 +1,95 @@
+
+products_list = [["iphone6",2000],
+            ["iphone7",3000],
+            ["iphone8",5000],
+            ["iphoneX",8000]]
+import sys
+import os
+import getpass
+count = 0
+flag = True
+bag = {}
+users = "user_file.txt"
+product = "products.txt"
+bag_data = "shopping_bag.txt"
+
+with open(bag_data) as fp_bag:
+    data_tmp = fp_bag.read().strip()
+    bag = eval(data_tmp)
+#print(bag)
+
+while flag:
+    user_name = input(">>UserName:").strip()
+    with open(users) as fp:
+        for line in fp:
+            #print(line)
+            user_tmp,passwd_tmp,balance = line.split()
+            print(user_tmp,passwd_tmp,balance)
+            balance = int(balance)
+            if user_name == user_tmp:
+                password = getpass.getpass(">>PassWord:").strip()
+                if password == passwd_tmp:
+                    print(">>WELCOME!Your balance:%s￥<<"%balance)
+                    with open(bag_data) as fp3:
+                        data_tmp = eval(fp3.read().strip())
+                        data_user = data_tmp[user_name]
+                        print("Shopping History:\n"+str(data_user))
+                        flag = False
+                else:
+                    print("user name or password is wrong,please try again！")
+while True:
+    id = 1
+    print("You can buy:：")
+    for i in products_list:
+        print(id,i[0],i[1])
+        id += 1
+    choise = input(">>Input your choise or "q" to exit:).strip()
+    if choise.isdigit():
+        choise = int(choise)
+        if choise >=1 and choise < id:
+            p_name = products_list[choise-1][0]
+            p_price = products_list[choise-1][1]
+            if balance < p_price:
+                diff = p_price - balance
+                yn = input(">>余额不足，是否进行充值(y/n)？:").strip()
+                if yn == "y":
+                    top_up = int(input(">>充值金额：").strip())
+                    balance  += top_up
+                elif yn == 'n':
+                    choise = "q"
+                else:
+                    print("输入错误！")
+
+            else:
+                prodct_key = bag[user_name].keys()
+                if p_name in prodct_key:
+                    bag[user_name][p_name][1] += 1
+                else:
+                    bag[user_name][p_name] = [p_price,1]
+                balance -= p_price
+                print("%s已经加入购物车"%p_name)
+                print("已经购买的商品：",bag[user_name],"余额%s"%balance)
+        else:
+            print("请输入正确的商品编号或退出！")
+    elif choise == 'q':
+        id_count = 1
+        print(bag)
+        with open("shopping_bag.txt","w") as fp2:
+             fp2.write(str(bag))
+        print("************Your shopping list*********")
+        print("id    products    quantity    price    total")
+        for key in bag[user_name].keys():
+            msg = "%s     %s       %s         %s     %s"%(id_count,
+                                                          key,
+                                                          bag[user_name][key][1],
+                                                          bag[user_name][key][0],
+                                                          bag[user_name][key][0]*bag[user_name][key][1])
+            print(msg)
+            id_count += 1
+        print("Your current Balance:",balance)
+        print("*****************end*******************")
+        break
+
+
+    else:
+        print("请输入正确的产品编号！")
