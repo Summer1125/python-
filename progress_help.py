0ef847e9f0469acba13053200d9c5d7f61b68c53
Summer <xiaoqisl@iCloud.com>
2017-10-23 22:14:35 +0800

    Update progress_help.py
    协程函数语法，以及如何初始化装饰器

--- progress_help.py
+++ progress_help.py
@@ -14,3 +14,24 @@ e.send("apple")
 e.send("banana")
 e.send("orange")
 print(next(e))
+
+##给协程函数初始化装饰器
+def init(func):
+    def wrapper(*args,**kwargs):
+        res = func(*args,**kwargs)
+        next(res)
+        return res
+    return wrapper
+
+@init #eater = init(eater)
+def eater(name):
+    print("%s start to eat "%name)
+    food_list=[]
+    while True:
+        food = yield food_list
+        print("%s get %s ,eating"%(name,food))
+        food_list.append(food)
+
+e = eater("Jack")
+print(e.send("apple"))
+print(e.send("banana"))
