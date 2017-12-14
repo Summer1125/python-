ab2ee5d34b197504f8509d6dc2db03943378d979
Summer <xiaoqisl@iCloud.com>
2017-12-14 18:11:45 +0800

    Add files via upload

---
+++ NOTE/Object_Orien/SeleCourse.py
@@ -0,0 +1,12 @@
+#!-*- coding: utf-8 -*-
+import pickle
+import admin
+import student
+course_file = "....."
+msg = '''*********欢迎进入选课系统************
+    请选择登录方式：
+        1、管理员
+        2、学生
+**************************************'''
+print(msg)
+
